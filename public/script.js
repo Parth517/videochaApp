6f6fd85fce7b3334138f5f764dfa8d670d124b57
Parth Deshpande <parthjun27@gmail.com>
2024-03-26 20:35:25 +0530

    Connecting to server

--- public/script.js
+++ public/script.js
@@ -1,2 +1,12 @@
 const socket =io('/')
 
+const myPeer=new Peer(undefined,{
+    host:'/',
+    port:'3001'
+})
+//use peer js
+socket.emit('join-room',Room_Id,10)
+
+socket.on('user-connected',userId=>{
+    console.log('User Connected'+ userId);
+})
